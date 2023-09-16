3e0406fe47a8f4a2e012c137ed538095e43c5473
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-09-16 14:21:51 -0300

    Finalizando aula da uri 2990

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,11 +1,15 @@
 package com.devsuperior.uri2990;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2990Application implements CommandLineRunner {
@@ -19,6 +23,20 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		System.out.println("SQL");
+		var listEmployessNoProjects = repository.listEmployeeNoPojects().stream().map(EmpregadoDeptDTO::new).collect(Collectors.toList());
+
+		for (EmpregadoDeptDTO list : listEmployessNoProjects) {
+			System.out.println(list);
+		}
+
+		System.out.println("JPQL");
+		var listEmployessNoProjectsJPQL = repository.listEmployeeNoPojectsJPQL();
+		for (EmpregadoDeptDTO list : listEmployessNoProjectsJPQL) {
+			System.out.println(list);
+		}
+
 		
 	}
 }
--- uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,23 @@
 package com.devsuperior.uri2990.repositories;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2990.entities.Empregado;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT e.cpf, e.enome, d.dnome FROM empregados e\n" +
+            "INNER JOIN departamentos d ON d.dnumero = e.dnumero\n" +
+            "WHERE NOT EXISTS ( \n" +
+            "\tSELECT * FROM trabalha WHERE cpf_emp = e.cpf\n" +
+            ") ORDER BY e.cpf")
+    List<EmpregadoDeptProjection> listEmployeeNoPojects();
+
+   
 
 }
